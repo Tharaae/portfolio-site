4abd1e4f12fb268ecf89dd0b9ad780afc875d868
Tharaae <36555311+Tharaae@users.noreply.github.com>
2018-03-16 02:58:30 +1100

    changes to add the menu bar bottom border functionality

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
-/* This function shows the site logo inside the naviagetion bar while scrolling down the site */
+/* This function shows the site logo inside the naviagetion bar and the nav bar bottom border
+   while scrolling down the site */
 $(function() {
   // When any scrolling takes place in the window on this document
   $(window).scroll(function() {
-    //if the header section is scrolled up
-    if ($(window).scrollTop() > $('header').height()) {
+    //if the top of the home image is higher than the nav bar bottom border
+    if ($('#nav-border').offset().top > $('#home-image-container').offset().top) {
       //show the logo in the nav bar
-      $('#nav-logo').removeClass('hide-logo');
-      $('#nav-logo').addClass('show-logo');
-    } else { //if the header section is currently displayed
+      $('#nav-logo').removeClass('hide');
+    } else {//if the top of the home image is lower than the nav bar bottom border
       //hide the logo from the nav bar
-      $('#nav-logo').removeClass('show-logo');
-      $('#nav-logo').addClass('hide-logo');
+      $('#nav-logo').addClass('hide');
+    }
+
+    //if the image border is the same position as or higher than the nav bar border
+    if ($('#nav-border').offset().top >= $('#image-border').offset().top) {
+      //show the bottom border of the nav bar
+      $('#nav-border').removeClass('hide');
+    } else { //if the image border is lower than the nav bar border
+      //hide the bottom border of the nav bar
+      $('#nav-border').addClass('hide');
     }
   });
 });
